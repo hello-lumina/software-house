5627d515b56e10eb7c7aaf0adaf03b41d8cb943c
TheThorsy <thethorsy@gmail.com>
2023-11-21 18:35:19 +0100

    Nuxt initilized

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <body>
+        <header>
+            <marquee direction="right">Witaj, na naszej stronie głównej!</marquee>
+        </header>
+
+            <main>
+
+            </main>
+    </body>
+</template>
+
+<style>
+    * {
+        margin: 0;
+      }
+
+    header {
+        color: white;
+        font-size: 30px;
+        font-family: 'Times New Roman', Times, serif;
+        background-color: #333;
+        padding: 10px;
+           }
+
+</style>
+
